e178980d5ca135efbc936fd5a7001bc5087578ad
MOEAr4hur <peiyi626ding@gmail.com>
2023-02-10 21:47:22 -0500

    change all id in mock.sql based on the new rule of id creation

--- sql/mock.sql
+++ sql/mock.sql
@@ -1,44 +1,44 @@
 INSERT INTO user(user_id, user_name, password, full_name, gender, email_address, phone_number, address)
-    value (2302092140111111, 'tester00', 'cPw1898Mrqie8Eem6GqwNw==', 'Aa Testa', 'male', 'testa@example.com',
+    value (2023021021401001, 'tester00', 'cPw1898Mrqie8Eem6GqwNw==', 'Aa Testa', 'male', 'testa@example.com',
            '+10000000000', '401 Sunset Ave, Windsor, ON N9B 3P4');
 INSERT INTO user(user_id, user_name, password, full_name, gender, email_address, phone_number, address)
-    value (2302092140222222, 'tester01', 'cPw1898Mrqie8Eem6GqwNw==', 'Bb Testb', 'female', 'testb@example.com',
+    value (2023021021401002, 'tester01', 'cPw1898Mrqie8Eem6GqwNw==', 'Bb Testb', 'female', 'testb@example.com',
            '+21111111111', '401 Sunset Ave, Windsor, ON N9B 3P4');
 INSERT INTO user(user_id, user_name, password, full_name, gender, email_address, phone_number)
-    value (2302092140333333, 'tester02', 'cPw1898Mrqie8Eem6GqwNw==', 'Cc Testc', 'universal', 'testc@example.com',
+    value (2023021021401003, 'tester02', 'cPw1898Mrqie8Eem6GqwNw==', 'Cc Testc', 'universal', 'testc@example.com',
            '+32222222222');
 INSERT INTO user(user_id, user_name, password, full_name, gender, email_address, phone_number)
-    value (2302092140444444, 'tester03', 'cPw1898Mrqie8Eem6GqwNw==', 'Dd Testd', 'unknown', 'testd@example.com',
+    value (2023021021401004, 'tester03', 'cPw1898Mrqie8Eem6GqwNw==', 'Dd Testd', 'unknown', 'testd@example.com',
            '+8612345678901');
 
 INSERT INTO meal(meal_id, user_id, meal_type, total_weight, total_calories)
-    value (2302092140444445, 2302092140111111, 'breakfast', 780, 800);
+    value (2023020921401001, 2023021021401001, 'breakfast', 600000, 43800);
 INSERT INTO meal(meal_id, user_id, meal_type, total_weight, total_calories)
-    value (2302092140444446, 2302092140111111, 'lunch', 800, 870);
+    value (2023020921401002, 2023021021401001, 'lunch', 600000, 43700);
 INSERT INTO meal(meal_id, user_id, meal_type, total_weight, total_calories)
-    value (2302092140444447, 2302092140222222, 'dinner', 570, 650);
+    value (2023020921401003, 2023021021401002, 'dinner', 570, 650);
 INSERT INTO meal(meal_id, user_id, meal_type, total_weight, total_calories)
-    value (2302092140444448, 2302092140222222, 'snack', 300, 400);
+    value (2023020921401004, 2023021021401002, 'snack', 300, 400);
 
 INSERT INTO ingredient(ingredient_id, ingredient_name, description, calories)
-    value (2302092140477448, 'test_food_A', 'apple', 100);
+    value (2023020821401001, 'test_food_A', 'apple', 100);
 INSERT INTO ingredient(ingredient_id, ingredient_name, description, calories)
-    value (2302092140477421, 'test_food_B', 'Banana', 70);
+    value (2023020821401002, 'test_food_B', 'Banana', 70);
 INSERT INTO ingredient(ingredient_id, ingredient_name, description, calories)
-    value (2302092140477445, 'test_food_C', 'coconut', 66);
+    value (2023020821401003, 'test_food_C', 'coconut', 66);
 INSERT INTO ingredient(ingredient_id, ingredient_name, description, calories)
-    value (2302092140477423, 'test_food_D', 'Dog', 99);
+    value (2023020821401004, 'test_food_D', 'Dog', 99);
 
 INSERT INTO meal_detail(detail_id, meal_id, ingredient_id, user_id, weight, calories)
-    value (2302092240444445, 2302092140444445, 2302092140477448, 2302092140111111, 100000, 10000);
+    value (2023021021411001, 2023020921401001, 2023020821401001, 2023021021401001, 100000, 10000);
 INSERT INTO meal_detail(detail_id, meal_id, ingredient_id, user_id, weight, calories)
-    value (2302092240443345, 2302092140444445, 2302092140477421, 2302092140111111, 200000, 14000);
+    value (2023021021411002, 2023020921401001, 2023020821401002, 2023021021401001, 200000, 14000);
 INSERT INTO meal_detail(detail_id, meal_id, ingredient_id, user_id, weight, calories)
-    value (2302092240442245, 2302092140444445, 2302092140477445, 2302092140111111, 300000, 19800);
+    value (2023021021411003, 2023020921401001, 2023020821401003, 2023021021401001, 300000, 19800);
 
 INSERT INTO meal_detail(detail_id, meal_id, ingredient_id, user_id, weight, calories)
-    value (2302082240442245, 2302092140444446, 2302092140477421, 2302092140111111, 200000, 14000);
+    value (2023021021411004, 2023020921401002, 2023020821401002, 2023021021401001, 200000, 14000);
 INSERT INTO meal_detail(detail_id, meal_id, ingredient_id, user_id, weight, calories)
-    value (2202082240442245, 2302092140444446, 2302092140477445, 2302092140111111, 300000, 19800);
+    value (2023021021411005, 2023020921401002, 2023020821401003, 2023021021401001, 300000, 19800);
 INSERT INTO meal_detail(detail_id, meal_id, ingredient_id, user_id, weight, calories)
-    value (2102082240442245, 2302092140444446, 2302092140477423, 2302092140111111, 100000, 9900);
+    value (2023021021411006, 2023020921401002, 2023020821401004, 2023021021401001, 100000, 9900);
