14de4bbb8879c0e28cb2c0c0b91f5fdcb0f3c8cd
vskendo <smartcrazylin@gmail.com>
2023-02-24 13:49:05 -0500

    Change the standard after negotiating with wei Ler

--- src/main/java/com/group42/model/valid/Info.java
+++ src/main/java/com/group42/model/valid/Info.java
@@ -2,9 +2,5 @@ package com.group42.model.valid;
 
 import javax.validation.groups.Default;
 
-/**
- * @auther zhouzhifeng
- * @date 2022/7/18
- */
 public interface Info extends Default {
 }
--- src/main/java/com/group42/model/valid/Merge.java
+++ src/main/java/com/group42/model/valid/Merge.java
@@ -2,10 +2,6 @@ package com.group42.model.valid;
 
 import javax.validation.groups.Default;
 
-/**
- * @auther zhouzhifeng
- * @date 2022/7/29
- */
 public interface Merge extends Default {
 
 }
